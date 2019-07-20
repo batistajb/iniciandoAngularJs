ce47b98806f2ca26c3a1bbdc9b5f9976342b747c
joao.batista <joao.batista@cromg.org.br>
2019-07-20 18:20:01 -0300

    first commit

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,22 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+	
+	$scope.fotos =[];
+
+	var promise = $http.get('v1/fotos');
+
+	$http.get('v1/fotos')
+		.success(function (fotos) {
+			$scope.fotos = fotos;
+		})
+		.error(function (error) {
+			console.log(error);
+		});
+
+
+	/*promise.then(function (retorno) {
+		$scope.fotos = retorno.data;
+	}).catch(function (error) {
+		console.log(error);
+	})*/
+
+});
